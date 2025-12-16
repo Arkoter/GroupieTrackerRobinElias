e23becba918e86f15ddae67f758b3f315480fd0e
robin <robin.martin67@ynov.com>
2025-12-16 16:34:32 +0100

    ajout page artiste liste

--- main.go
+++ main.go
@@ -4,15 +4,28 @@ import (
 	"fmt"
 	"html/template"
 	"net/http"
+
+	"groupie-tracker/api"
 )
 
 func indexHandler(w http.ResponseWriter, r *http.Request) {
+	artists := api.FetchArtists()
+
 	tmpl, _ := template.ParseFiles("templates/index.html")
-	tmpl.Execute(w, nil)
+	tmpl.Execute(w, artists)
+}
+
+func artistsHandler(w http.ResponseWriter, r *http.Request) {
+	artists := api.FetchArtists()
+
+	tmpl, _ := template.ParseFiles("templates/artists.html")
+	tmpl.Execute(w, artists)
 }
 
 func main() {
 	http.HandleFunc("/", indexHandler)
+	http.HandleFunc("/artists", artistsHandler)
+
 	fmt.Println("Démarrage du serveur sur http://localhost:8080")
 	http.ListenAndServe(":8080", nil)
 }
