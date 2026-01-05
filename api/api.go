9bc7ff67373a192f5a77471ae0c0883329926e6a
el211 <saireelias@gmail.com>
2026-01-05 12:38:16 +0100

    ajoute de letape 2 et 3

--- api/api.go
+++ api/api.go
@@ -2,25 +2,106 @@ package api
 
 import (
 	"encoding/json"
+	"fmt"
 	"net/http"
 )
 
 type Artist struct {
-	ID            int      `json:"id"`
-	Image         string   `json:"image"`
-	Name          string   `json:"name"`
-	CreationDate  int      `json:"creationDate"`
-	Members       []string `json:"members"`
+	ID           int      `json:"id"`
+	Image        string   `json:"image"`
+	Name         string   `json:"name"`
+	CreationDate int      `json:"creationDate"`
+	Members      []string `json:"members"`
+}
+
+type Locations struct {
+	Locations []string `json:"locations"`
+}
+
+type Dates struct {
+	Dates []string `json:"dates"`
+}
+
+type Relation struct {
+	DatesLocations map[string][]string `json:"datesLocations"`
+}
+
+type ArtistDetail struct {
+	Artist
+	FirstAlbum     string
+	Locations      []string
+	Dates          []string
+	DatesLocations map[string][]string
 }
 
 const artistsAPI = "https://groupietrackers.herokuapp.com/api/artists"
+const locationsAPI = "https://groupietrackers.herokuapp.com/api/locations"
+const datesAPI = "https://groupietrackers.herokuapp.com/api/dates"
+const relationAPI = "https://groupietrackers.herokuapp.com/api/relation"
 
 func FetchArtists() []Artist {
-	resp, _ := http.Get(artistsAPI)
+	resp, err := http.Get(artistsAPI)
+	if err != nil {
+		return nil
+	}
 	defer resp.Body.Close()
 
 	var artists []Artist
 	json.NewDecoder(resp.Body).Decode(&artists)
-
 	return artists
+}
+
+func FetchArtistDetail(id int) *ArtistDetail {
+	// Récupairer lartist de base
+	artists := FetchArtists()
+	var artist Artist
+	found := false
+	for _, a := range artists {
+		if a.ID == id {
+			artist = a
+			found = true
+			break
+		}
+	}
+	if !found {
+		return nil
+	}
+
+	// Récup les locations
+	locResp, err := http.Get(fmt.Sprintf("%s/%d", locationsAPI, id))
+	if err != nil {
+		return nil
+	}
+	defer locResp.Body.Close()
+
+	var locations Locations
+	json.NewDecoder(locResp.Body).Decode(&locations)
+
+	// Récup les date
+	dateResp, err := http.Get(fmt.Sprintf("%s/%d", datesAPI, id))
+	if err != nil {
+		return nil
+	}
+	defer dateResp.Body.Close()
+
+	var dates Dates
+	json.NewDecoder(dateResp.Body).Decode(&dates)
+
+	// Récup les relations
+	relResp, err := http.Get(fmt.Sprintf("%s/%d", relationAPI, id))
+	if err != nil {
+		return nil
+	}
+	defer relResp.Body.Close()
+
+	var relation Relation
+	json.NewDecoder(relResp.Body).Decode(&relation)
+
+	return &ArtistDetail{
+		Artist:         artist,
+		FirstAlbum:     "",
+		Locations:      locations.Locations,
+		Dates:          dates.Dates,
+		DatesLocations: relation.DatesLocations,
+	}
 }
--- main.go
+++ main.go
@@ -4,6 +4,10 @@ import (
 	"fmt"
 	"html/template"
 	"net/http"
+	"strconv"
+	"strings"
+
+	"groupie-tracker/api"
 )
 
 func indexHandler(w http.ResponseWriter, r *http.Request) {
@@ -11,8 +15,43 @@ func indexHandler(w http.ResponseWriter, r *http.Request) {
 	tmpl.Execute(w, nil)
 }
 
+func artistsHandler(w http.ResponseWriter, r *http.Request) {
+	artists := api.FetchArtists()
+	tmpl, err := template.ParseFiles("templates/artists.html")
+	if err != nil {
+		http.Error(w, "Erreur de template", http.StatusInternalServerError)
+		return
+	}
+	tmpl.Execute(w, artists)
+}
+
+func artistDetailHandler(w http.ResponseWriter, r *http.Request) {
+	path := strings.TrimPrefix(r.URL.Path, "/artist/")
+	id, err := strconv.Atoi(path)
+	if err != nil {
+		http.Error(w, "ID invalide", http.StatusBadRequest)
+		return
+	}
+
+	artistDetail := api.FetchArtistDetail(id)
+	if artistDetail == nil {
+		http.Error(w, "Artiste non trouvé", http.StatusNotFound)
+		return
+	}
+
+	tmpl, err := template.ParseFiles("templates/artist_detail.html")
+	if err != nil {
+		http.Error(w, "Erreur de template", http.StatusInternalServerError)
+		return
+	}
+	tmpl.Execute(w, artistDetail)
+}
+
 func main() {
 	http.HandleFunc("/", indexHandler)
+	http.HandleFunc("/artists", artistsHandler)
+	http.HandleFunc("/artist/", artistDetailHandler)
+
 	fmt.Println("Démarrage du serveur sur http://localhost:8080")
 	http.ListenAndServe(":8080", nil)
 }
